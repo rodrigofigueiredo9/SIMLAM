09604fcf09294066c397abe605ea848f2d8f5d74
Fred Lopes <frederico.lopes@pentago.com.br>
2016-12-14 11:47:24 -0200

    Arquivos da view model.

---
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Interno/ViewModels/VMConfiguracaoVegetal/DeclaracaoAdicionalVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tecnomapas.Blocos.Entities.Interno.ModuloVegetal;
+using Tecnomapas.Blocos.Entities.Interno.ModuloVegetal.DeclaracaoAdicional;
+using Tecnomapas.Blocos.Etx.ModuloValidacao;
+
+namespace Tecnomapas.EtramiteX.Interno.ViewModels.VMConfiguracaoVegetal
+{
+    public class DeclaracaoAdicionalVM
+    {
+        private List<DeclaracaoAdicional> _itens = new List<DeclaracaoAdicional>();
+
+        private DeclaracaoAdicional _declaracao = new DeclaracaoAdicional();
+
+        public DeclaracaoAdicional DeclaracaoAdicional {
+            get { return _declaracao; }
+            set { _declaracao = value; } 
+        }
+
+        public List<DeclaracaoAdicional> Itens
+        {
+            get { return _itens; }
+            set { _itens = value; }
+        }
+
+    
+    }
+}
