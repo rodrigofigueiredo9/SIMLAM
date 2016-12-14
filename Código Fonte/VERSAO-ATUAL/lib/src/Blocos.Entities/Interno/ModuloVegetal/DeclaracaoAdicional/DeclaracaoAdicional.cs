a18721e4f75f9fbc8393b40e479a2d104060976b
Fred Lopes <frederico.lopes@pentago.com.br>
2016-12-14 11:51:17 -0200

    Classe DeclaracaoAdicional.

---
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloVegetal/DeclaracaoAdicional/DeclaracaoAdicional.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tecnomapas.Blocos.Entities.Interno.ModuloVegetal.DeclaracaoAdicional
+{
+    public class DeclaracaoAdicional
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public string TextoFormatado { get; set; }
+        public int OutroEstado { get; set; }
+        public bool chkOutroEstado { get; set; }
+    }
+}
