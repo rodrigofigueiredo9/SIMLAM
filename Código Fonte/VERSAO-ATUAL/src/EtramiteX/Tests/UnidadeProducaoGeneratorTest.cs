a220ecf568abeca2568258ddd16f7a9eab8c8668
Armando Magalhães <armando.magalhaes@PENTAGO.BR>
2016-12-15 12:59:16 -0200

    test(UnidadeProducaoGenerator): Testes de geração de códigos
    Testes para validar a geração de códigos de propriedade e unidade de produção sob o novo padrão
    
    imposto pela RQF-21

---
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Tests/UnidadeProducaoGeneratorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tecnomapas.EtramiteX.Interno.Model.Extensoes.Caracterizacoes.ModuloUnidadeProducao.Business;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnidadeProducaoGeneratorTest
+    {
+        UnidadeProducaoGenerator generator;
+
+        [TestInitialize()]
+        public void initialize()
+        {
+            generator = new UnidadeProducaoGenerator();
+        }
+
+        [TestMethod]
+        public void DeveRetornarCodigoDaPropriedadeCom11Digitos()
+        {
+            Int64 result = generator.GerarCodigoPropriedade(3205200, 0001);
+            Int64 expected = 32052000001;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeveRetornarCodigoDaUPCom17Digitos()
+        {
+            Int64 result = generator.GerarCodigoUnidadeProducao(32052000001, 16, 0001);
+            Int64 expected = 32052000001160001;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeveRetornarSequencialCom4Digitos()
+        {
+            int sequencial = 32;
+            String expected = "0032";
+
+            Assert.AreEqual(expected, UnidadeProducaoGenerator.GetPaddedSequencial(sequencial));
+        }
+    }
+}
