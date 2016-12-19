074c9068fd334bc8dcee7a4e35801519d8ef5197
Fred Lopes <frederico.lopes@pentago.com.br>
2016-12-19 15:51:02 -0200

    Acerto validação.

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloVegetal/Business/CulturaValidar.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloVegetal/Business/CulturaValidar.cs
@@ -62,7 +62,7 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloVegetal.Business
 			{
 				lista.ForEach(x =>
 				{
-					if (x.PragaId == item.PragaId && x.TipoProducaoId == item.TipoProducaoId)
+					if (x.PragaId == item.PragaId && x.TipoProducaoId == item.TipoProducaoId && x.OutroEstado == item.OutroEstado )
 					{
 						Validacao.Add(Mensagem.CultivarConfiguracaoMsg.DeclaracaoJaAdicionado);						
 					}
