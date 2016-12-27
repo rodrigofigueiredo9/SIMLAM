f9a21825c66781894613e64c3ba3e6ac00f8bdb0
Fred Lopes <frederico.lopes@pentago.com.br>
2016-12-27 11:16:26 -0200

    Acrescentado arquivo.

---
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloPTVOutro/PTVOutroDeclaracao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tecnomapas.Blocos.Entities.Interno.ModuloPTV;
+
+namespace Tecnomapas.Blocos.Entities.Interno.ModuloPTVOutro
+{
+    public class PTVOutroDeclaracao
+    {
+        public int  IdPraga { get; set;}
+        public string NomeCientifico { get; set; }
+        public string NomeComum { get; set; }
+        public string DeclaracaoAdicional { get; set; }
+        public int IdDeclaracao { get; set; }
+        public int IdCultivar { get; set; }
+
+    }
+}
