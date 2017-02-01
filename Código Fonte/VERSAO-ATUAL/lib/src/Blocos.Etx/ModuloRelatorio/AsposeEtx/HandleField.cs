2ff12642486ebfa4c1d32bd877482160251b8dfa
Fred Lopes <frederico.lopes@pentago.com.br>
2017-02-01 12:20:09 -0200

    fix formatação declaração adicional.

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Etx/ModuloRelatorio/AsposeEtx/HandleField.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Etx/ModuloRelatorio/AsposeEtx/HandleField.cs
@@ -90,30 +90,30 @@ namespace Tecnomapas.Blocos.Etx.ModuloRelatorio.AsposeEtx
           
 
 
-            if (args.DocumentFieldName.EndsWith("Html"))
-            {
+            //if (args.DocumentFieldName.EndsWith("Html"))
+            //{
 
-                // Insert the text for this merge field as HTML data, using DocumentBuilder.
-                DocumentBuilder builder = new DocumentBuilder(args.Document);
-                builder.MoveToMergeField(args.DocumentFieldName);
-                builder.InsertHtml((string)args.FieldValue);
+            //    // Insert the text for this merge field as HTML data, using DocumentBuilder.
+            //    DocumentBuilder builder = new DocumentBuilder(args.Document);
+            //    builder.MoveToMergeField(args.DocumentFieldName);
+            //    builder.InsertHtml((string)args.FieldValue);
 
-                while (!builder.CurrentParagraph.HasChildNodes)
-                {
-                    // Get previouse node.
-                    Node prevNode = builder.CurrentParagraph.PreviousSibling;
-                    // we do not need to remove current paragraph if th epreviouse node is table for example.
-                    if (prevNode == null || prevNode.NodeType != NodeType.Paragraph)
-                        break;
+            //    while (!builder.CurrentParagraph.HasChildNodes)
+            //    {
+            //        // Get previouse node.
+            //        Node prevNode = builder.CurrentParagraph.PreviousSibling;
+            //        // we do not need to remove current paragraph if th epreviouse node is table for example.
+            //        if (prevNode == null || prevNode.NodeType != NodeType.Paragraph)
+            //            break;
 
-                    builder.CurrentParagraph.Remove();
-                    builder.MoveTo(prevNode);
-                }
+            //        builder.CurrentParagraph.Remove();
+            //        builder.MoveTo(prevNode);
+            //    }
 
-                // The HTML text itself should not be inserted.
-                // We have already inserted it as an HTML.
-                args.Text = "";
-            }
+            //    // The HTML text itself should not be inserted.
+            //    // We have already inserted it as an HTML.
+            //    args.Text = "";
+            //}
            
 		}
 
