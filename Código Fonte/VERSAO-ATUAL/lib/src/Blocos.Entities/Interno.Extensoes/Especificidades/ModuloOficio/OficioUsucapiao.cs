da0d0fb7e62ae1413bf4c92ef9e810eb9d3234ba
Fred Lopes <frederico.lopes@pentago.com.br>
2017-06-08 12:11:18 -0300

    Acertos na classe de usucapião.

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno.Extensoes/Especificidades/ModuloOficio/OficioUsucapiao.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno.Extensoes/Especificidades/ModuloOficio/OficioUsucapiao.cs
@@ -27,8 +27,8 @@ namespace Tecnomapas.Blocos.Entities.Interno.Extensoes.Especificidades.ModuloOfi
 			{
 				return @"À senhora,
 Roberta Beatriz Teodoro Souza
-Chefe de Setorial – PPI/PGE
-Vitória – ES";
+Chefe de Setorial - PPI/PGE
+Vitória - ES";
 			}
 		}
 
@@ -38,7 +38,16 @@ Vitória – ES";
 			{
 				return @"Senhora chefe:
  
-Trata o presente ofício de resposta ao Ofício PGE/PPI Nº <<---/---- >>, referente a ação de usucapião proposta por << --- >>, processo judicial Nº << --- >> no qual está sendo reivindicada a regularização da área com a dimensão de << --- >> m², ";
+Trata o presente ofício de resposta ao Ofício PGE/PPI Nº <<---/---- >>, referente a ação de usucapião proposta por << --- >>, processo judicial Nº << --- >> no qual está sendo reivindicada a regularização da área com a dimensão de << --- >> m², localizado no lugar denominado << ---- >>, conforme petição e planta anexas.
+
+01.  De acordo com a planta apresentada, verifica-se em nosso sistema cadastral de imóveis, que a área objeto da presente Ação de Usucapião não está localizada em glebas discriminadas e classificada como bem dominial devoluto e nem em glebas de bem patrimonial em nome do Estado do Espírito Santo.
+
+02.  Informo também que o imóvel objeto da Ação não é confinante com nenhum imóvel de domínio do Estado do Espírito Santo.
+ 
+03.  Não é de conhecimento público a existência de interesse do Estado para destinação de uso público ou social. Sendo assim, opinamos pela continuidade do processo em referência.
+
+04.  Ficam, entretanto, ressalvados os bens imóveis de domínio do Estado do Espírito Santo que venham a ser posteriormente, por quaisquer medidas, identificados na área em causa ou em sua confrontação.
+";
 			}
 		}
 	}
