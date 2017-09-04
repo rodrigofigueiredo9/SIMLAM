abdda2bf4c920b9aa1f0eda755aafd2730316912
patrick.vianna <patrick.vianna@pentago.com.br>
2017-09-04 10:40:20 -0300

    feat(17-RQF-05): Corrigido a paginação em institucional

--- Código Fonte/VERSAO-ATUAL/src/Credenciado/Credenciado/Scripts/CFOCFOC/consultarCfoCfoc.js
+++ Código Fonte/VERSAO-ATUAL/src/Credenciado/Credenciado/Scripts/CFOCFOC/consultarCfoCfoc.js
@@ -110,8 +110,6 @@ ConsultarNumeroCFOCFOCLiberado = {
 
 		$(container).find('.numerosPag').empty();
 
-		
-
 		if (qtdPaginas <= 10) {						
 		    for (var i = 0; i < qtdPaginas; i++) {
 		        $(container).find('.numerosPag').append("<a class='" + (i + 1) + " paginar pag'>" + (i + 1) + "</a>");
@@ -128,9 +126,7 @@ ConsultarNumeroCFOCFOCLiberado = {
 		    } else {
 		        for (i = numeroPagina - 5; i < numeroPagina + 4; i++) {
 		            $(container).find('.numerosPag').append("<a class='" + (i + 1) + " paginar pag'>" + (i + 1) + "</a>");
-
 		        }
-		        console.log("ENTROU");
 		    }																
 		}
 
