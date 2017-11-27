345b1cf1435b6248aa24cb164c92c51ed2857b27
cinthia.aguiar <cinthia.aguiar@pentago.com.br>
2017-11-27 10:54:37 -0200

    Alteração de nome na pasta de scripts

---
+++ Queries/IDAF-0017 - Numeracao CFO CFOC/credenciado_altertables.sql
@@ -0,0 +1,11 @@
+ALTER TABLE idafcredenciado.tab_cfo
+  ADD( serie varchar1(1) );
+  
+ALTER TABLE idafcredenciado.hst_cfo
+  ADD( serie varchar1(1) );
+  
+ALTER TABLE idafcredenciado.tab_cfoc
+  ADD( serie varchar1(1) );
+  
+ALTER TABLE idafcredenciado.hst_cfoc
+  ADD( serie varchar1(1) );
