00929f4749570042dec0e22d29eb02dee89294f2
cinthia.aguiar <cinthia.aguiar@pentago.com.br>
2018-01-04 15:00:55 -0200

    fix(2457): Correções de erros relacionados à liberação de CFO/CFOC
    Pequenos erros na mensagem de validação exibida, não prejudica a liberação de números.

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloCredenciado/Data/LiberacaoNumeroCFOCFOCDa.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloCredenciado/Data/LiberacaoNumeroCFOCFOCDa.cs
@@ -605,6 +605,16 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloCredenciado.Data
                 else
                 {
                     bancoDeDados.Rollback();
+
+                    if (tipoDocumento == 1)
+                    {
+                        Validacao.Add(Mensagem.LiberacaoNumeroCFOCFOC.QuantidadeNumDigitalCFOUltrapassaConfiguradaSistema);
+                    }
+
+                    if (tipoDocumento == 2)
+                    {
+                        Validacao.Add(Mensagem.LiberacaoNumeroCFOCFOC.QuantidadeNumDigitalCFOCUltrapassaConfiguradaSistema);
+                    }
                 }
             }
         }
@@ -1111,7 +1121,8 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloCredenciado.Data
                                                               and tnum.tipo_numero = 2 ) ) livres_intervalo
                                            from cnf_doc_fito_intervalo cnf
                                            where cnf.tipo_documento = 1
-                                                 and cnf.tipo = 2 )");
+                                                 and cnf.tipo = 2
+                                                 and substr(cnf.numero_inicial, 3, 2) = to_char(sysdate, 'YY') )");
 
                 int livres = 0;
 
@@ -1147,7 +1158,8 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloCredenciado.Data
                                                               and tnum.tipo_numero = 2 ) ) livres_intervalo
                                            from cnf_doc_fito_intervalo cnf
                                            where cnf.tipo_documento = 2
-                                                 and cnf.tipo = 2 )");
+                                                 and cnf.tipo = 2
+                                                 and substr(cnf.numero_inicial, 3, 2) = to_char(sysdate, 'YY') )");
 
                 int livres = 0;
 
