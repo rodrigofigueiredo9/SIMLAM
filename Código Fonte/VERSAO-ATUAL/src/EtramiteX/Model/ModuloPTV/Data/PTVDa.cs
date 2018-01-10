61566fc60e5482cdf0331f083b381edf99aa9cdc
cinthia.aguiar <cinthia.aguiar@pentago.com.br>
2018-01-10 10:40:36 -0200

    fix(2543): Correção do método do INSTITUCIONAL que gerava números de PTV
    Na hora de pegar o próxima número disponível para PTV digital, o sistema estava verificando apenas na tabela de PTV do institucional. Quando havia algum número cadastrado apenas na tabela de PTV do credenciado, gerava uma duplicação. É necessário verificar sempre nos dois bancos.

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloPTV/Data/PTVDa.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloPTV/Data/PTVDa.cs
@@ -829,6 +829,8 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloPTV.Data
 
 		internal string ObterNumeroDigital()
 		{
+            string retorno = string.Empty;
+
 			using (BancoDeDados bancoDeDados = BancoDeDados.ObterInstancia())
 			{
 				Comando comando = bancoDeDados.CriarComandoPlSql(@"
@@ -868,8 +870,61 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloPTV.Data
 				comando.AdicionarParametroSaida("numero_saida", DbType.Int64);
 				bancoDeDados.ExecutarNonQuery(comando);
 
-				return Convert.ToString(comando.ObterValorParametro("numero_saida"));
-			}
+                retorno = Convert.ToString(comando.ObterValorParametro("numero_saida"));
+            }
+
+            //Credenciado
+            using (BancoDeDados bancoDeDados = BancoDeDados.ObterInstancia(UsuarioCredenciado))
+            {
+                Comando comando = bancoDeDados.CriarComandoPlSql(@"
+				declare
+					v_aux   number := 0;
+					v_maior number := 0;
+				begin
+					select nvl((select max(d.numero) from tab_ptv d where d.tipo_numero = 2
+									and to_char(d.numero) like '__'|| to_char(sysdate, 'yy') ||'%'),
+						(select min(c.numero_inicial) - 1 from cnf_doc_fito_intervalo c where c.tipo_documento = 3 and c.tipo = 2
+									and to_char(c.numero_inicial) like '__'|| to_char(sysdate, 'yy') ||'%'))
+					into v_maior from dual;
+
+					v_maior := v_maior + 1;
+
+					select count(1) into v_aux from cnf_doc_fito_intervalo c 
+					where c.tipo_documento = 3 and c.tipo = 2 
+					and substr(c.numero_inicial, 3, 2) = substr(EXTRACT(YEAR FROM sysdate), 3) 
+					and substr(c.numero_final, 3, 2) = substr(EXTRACT(YEAR FROM sysdate), 3) 
+					and (v_maior between c.numero_inicial and c.numero_final);
+
+					if (v_aux <= 0) then
+						v_aux := v_maior;
+
+						select min(t.numero_inicial) into v_maior from cnf_doc_fito_intervalo t 
+						where t.tipo_documento = 3 and t.tipo = 2 
+						and substr(t.numero_inicial, 3, 2) = substr(EXTRACT(YEAR FROM sysdate), 3) 
+						and substr(t.numero_final, 3, 2) = substr(EXTRACT(YEAR FROM sysdate), 3)
+						and t.numero_inicial > v_maior;
+
+						if(v_maior is null or v_aux = v_maior) then 
+							v_maior := null;
+						end if;
+					end if;
+					select v_maior into :numero_saida from dual;
+				end;", UsuarioCredenciado);
+                comando.AdicionarParametroSaida("numero_saida", DbType.Int64);
+                bancoDeDados.ExecutarNonQuery(comando);
+
+                if (!Convert.IsDBNull(comando.ObterValorParametro("numero_saida")) && comando.ObterValorParametro("numero_saida") != null)
+                {
+                    string aux = Convert.ToString(comando.ObterValorParametro("numero_saida"));
+
+                    if (string.IsNullOrEmpty(retorno) || Convert.ToInt64(aux) > Convert.ToInt64(retorno))
+                    {
+                        retorno = aux;
+                    }
+                }
+            }
+
+            return retorno;
 		}
 
 		internal List<ListaValor> ObterResponsaveisEmpreendimento(int empreendimentoID)
