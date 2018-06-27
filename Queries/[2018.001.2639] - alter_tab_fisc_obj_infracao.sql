d08343b96b6e912f00fdbd35d3a0abd4a4f89813
jhonny.riley <jhonny.correa@pentago.com.br>
2018-06-27 09:59:13 -0300

    Versionamento de scripts

--- Queries/[2018.001.2639] - alter_tab_fisc_obj_infracao.sql
+++ Queries/[2018.001.2639] - alter_tab_fisc_obj_infracao.sql
@@ -2,3 +2,5 @@ alter table tab_fisc_obj_infracao modify DESC_TERMO_EMBARGO VARCHAR2(2000 BYTE);
 alter table hst_fisc_obj_infracao modify DESC_TERMO_EMBARGO VARCHAR2(2000 BYTE);
 alter table tab_fisc_obj_infracao modify opniao_area_danificada VARCHAR2(2000 BYTE);
 alter table hst_fisc_obj_infracao modify opniao_area_danificada VARCHAR2(2000 BYTE);
+
+UPDATE IDAF.CNF_IMPLANTACAO SET VALOR = '2018.001.2639' WHERE CAMPO like 'ultimoscript';
