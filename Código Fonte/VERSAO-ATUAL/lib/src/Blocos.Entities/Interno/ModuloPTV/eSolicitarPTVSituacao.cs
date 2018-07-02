506e20624353072fa44f7f5b57337588c51518f3
Patrick Vianna <patrickviannapblv@gmail.com>
2018-07-02 10:31:57 -0300

    23-RQF-05: Acertos
    1 - Mudança no filtro, agora é feito com subselect
    2 - Atualização do eNumerable
    3 - Exibição do botão comunicador
    4 - Mudança no filtro de situação

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloPTV/eSolicitarPTVSituacao.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloPTV/eSolicitarPTVSituacao.cs
@@ -3,11 +3,13 @@
 	public enum eSolicitarPTVSituacao
 	{
 		Nulo = 0,
-		Cadastrado,
-		AguardandoAnalise,
-		Aprovado,
-		Rejeitado,
-		AgendarFiscalizacao,
-		Bloqueado
+		Cadastrado = 1,
+		AguardandoAnalise = 2,
+		Válido = 3,
+		Rejeitado = 4,
+		AgendarFiscalizacao = 5,
+		Bloqueado = 6,
+		Inválido = 7,
+		Editado = 8
 	}
 }
