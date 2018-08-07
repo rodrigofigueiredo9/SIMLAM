5967c0d1a4bc0041b268dce219ff920ec260fc20
Patrick Vianna <patrickviannapblv@gmail.com>
2018-08-07 09:05:59 -0300

    dlls

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloCadastroAmbientalRural/Business/CARSolicitacaoBus.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloCadastroAmbientalRural/Business/CARSolicitacaoBus.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using System.Web;
 using Tecnomapas.Blocos.Data;
 using Tecnomapas.Blocos.Entities.Configuracao.Interno;
