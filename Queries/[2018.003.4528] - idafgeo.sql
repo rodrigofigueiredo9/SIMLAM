dfea3561f411fc12da32714d71cc012c01f2a56d
jhonny.riley <jhonny.correa@pentago.com.br>
2018-08-30 15:16:33 -0300

    [5049] - ajuste no processamento de geo

--- Queries/[2018.003.4528] - idafgeo.sql
+++ Queries/[2018.003.4528] - idafgeo.sql
@@ -23,11 +23,11 @@ CREATE TABLE "IDAFGEO"."LOV_TIPO_EXPLORACAO"
    COMMENT ON TABLE "IDAFGEO"."LOV_TIPO_EXPLORACAO"  IS 'Lista de valores para as tabelas do desenhador';
  
  
-insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto)
+insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto, tipo_atividade)
 values (1, 'AUS -  Uso Alternativo do Solo', 370);
-insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto)
+insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto, tipo_atividade)
 values (2, 'CAI - Corte de Árvore Isolada', 374);
-insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto)
+insert into idafgeo.LOV_TIPO_EXPLORACAO (chave, texto, tipo_atividade)
 values (3, 'EFP - Exploração de Floresta Plantada', 929);
 
 
@@ -36,116 +36,119 @@ insert into idafgeo.tab_feicao_colunas (feicao, coluna, tipo, tamanho, alias, is
 values (31,'TIPO_EXPLORACAO', 4,1,'Tipo de Exploração',1,1,0, 'LOV_TIPO_EXPLORACAO', 'CHAVE');
 
 -- ADD COLUMN TIPO_EXPLORACAO INTO TABELAS DO GEO
-alter table IDAFGEO.DES_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.DES_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."DES_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_DES_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_DES_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_DES_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."TMP_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.DES_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.DES_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."DES_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.DES_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.DES_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."DES_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_DES_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_DES_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_DES_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.TMP_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.TMP_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."TMP_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.TMP_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.TMP_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."TMP_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."GEO_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_GEO_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."GEO_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."GEO_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.HST_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.HST_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."HST_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.TMP_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.TMP_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."TMP_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.INV_TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.INV_TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."INV_TMP_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."GEO_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.HST_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.HST_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."HST_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.HST_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.HST_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."HST_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.HST_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.HST_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."HST_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_DES_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_DES_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_DES_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_DES_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_DES_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_DES_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.DES_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.DES_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."DES_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_GEO_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_GEO_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_GEO_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_TMP_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_TMP_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_TMP_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_TMP_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_TMP_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_TMP_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_TMP_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_TMP_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FE_TMP_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FE_TMP_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FE_TMP_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.VAL_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.VAL_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."VAL_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.VAL_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.VAL_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."VAL_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.VAL_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.VAL_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."VAL_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.VAL_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.VAL_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."VAL_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FEC_GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FEC_GEO_AATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FEC_GEO_AATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FEC_GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FEC_GEO_AIATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FEC_GEO_AIATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FEC_GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FEC_GEO_LATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FEC_GEO_LATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
-alter table IDAFGEO.FEC_GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(1 BYTE);
+alter table IDAFGEO.FEC_GEO_PATIV add TIPO_EXPLORACAO VARCHAR2(50 BYTE);
 COMMENT ON COLUMN "IDAFGEO"."FEC_GEO_PATIV"."TIPO_EXPLORACAO" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
 
 --PERMISSÃO DE CONSULTA PARA IDAF
 grant SELECT on "IDAFGEO"."LOV_TIPO_EXPLORACAO" to "IDAF" ;
+
+alter table IDAFGEO.TMP_RASC_TRACKMAKER add TIPOEXP VARCHAR2(50 BYTE);
+COMMENT ON COLUMN "IDAFGEO"."TMP_RASC_TRACKMAKER"."TIPOEXP" IS 'Texto do tipo de exploracao associado ao ponto de atividade.';
