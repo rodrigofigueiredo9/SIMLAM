4f4baaee1a8b99f364cb152c270428aa2e5c9821
John Riley <jhon_aiox@hotmail.com>
2018-11-21 19:08:00 -0200

    [0025-5498] - ajuste na validacao de laudo de exploracao / ajuste na geracao de PDF de croqui

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model.Extencao/Especificidades/ModuloLaudo/Business/LaudoVistoriaFlorestalValidar.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model.Extencao/Especificidades/ModuloLaudo/Business/LaudoVistoriaFlorestalValidar.cs
@@ -33,41 +33,17 @@ namespace Tecnomapas.EtramiteX.Interno.Model.Extensoes.Especificidades.ModuloLau
 			ValidacoesGenericasBus.DataMensagem(esp.DataVistoria, "Laudo_DataVistoria_DataTexto", "vistoria");
 
 			if (String.IsNullOrWhiteSpace(esp.Objetivo))
-			{
 				Validacao.Add(Mensagem.LaudoVistoriaFlorestalMsg.ObjetivoObrigatorio);
-			}
 
 			if (esp.Caracterizacao <= 0)
-			{
 				Validacao.Add(Mensagem.LaudoVistoriaFlorestalMsg.CaracterizacaoObrigatoria);
-			}
 			else
 			{
 				CaracterizacaoBus caracterizacaoBus = new CaracterizacaoBus();
 				int caracterizacao = caracterizacaoBus.Existe(esp.Titulo.EmpreendimentoId.GetValueOrDefault(), (eCaracterizacao)esp.Caracterizacao);
 
 				if (caracterizacao <= 0)
-				{
 					Validacao.Add(Mensagem.LaudoVistoriaFlorestalMsg.CaracterizacaoCadastrada);
-				}
-				else
-				{
-					CaracterizacaoValidar caracterizacaoValidar = new CaracterizacaoValidar();
-					List<Dependencia> dependencias = caracterizacaoBus.ObterDependencias(caracterizacao, 
-						(eCaracterizacao)esp.Caracterizacao, 
-						eCaracterizacaoDependenciaTipo.Caracterizacao);
-
-					string retorno = caracterizacaoValidar.DependenciasAlteradas(especificidade.Titulo.EmpreendimentoId.GetValueOrDefault(), 
-						esp.Caracterizacao, 
-						eCaracterizacaoDependenciaTipo.Caracterizacao, 
-						dependencias);
-
-					if (!string.IsNullOrEmpty(retorno))
-					{
-						List<CaracterizacaoLst> caracterizacoes = _caracterizacaoConfig.Obter<List<CaracterizacaoLst>>(ConfiguracaoCaracterizacao.KeyCaracterizacoes);
-						Validacao.Add(Mensagem.LaudoVistoriaFlorestalMsg.CaracterizacaoInvalida(caracterizacoes.SingleOrDefault(x => x.Id == esp.Caracterizacao).Texto));
-					}
-				}
 			}
 
 			if (String.IsNullOrWhiteSpace(esp.Consideracao))
