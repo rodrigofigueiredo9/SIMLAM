f13114ecaa79357d752e8bc6caf64dc465d9cd87
Patrick Vianna <patrickviannapblv@gmail.com>
2018-11-29 19:26:05 -0200

    23-RQM-08

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloPTV/NotaFiscalCaixa.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno/ModuloPTV/NotaFiscalCaixa.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Tecnomapas.Blocos.Entities.WebService;
 
 namespace Tecnomapas.Blocos.Entities.Interno.ModuloPTV
 {
@@ -16,6 +17,10 @@ namespace Tecnomapas.Blocos.Entities.Interno.ModuloPTV
 		public int numeroCaixas { get; set; }
 		public int tipoCaixaId { get; set; }
 		public string tipoCaixaTexto { get; set; }
+
+
+		public eTipoPessoa PessoaAssociadaTipo { get; set; }
+		public string PessoaAssociadaCpfCnpj { get; set; }
 
 		public NotaFiscalCaixa()
 		{
