9d6781e31816291fc7cab64af6c8cf1fdf570ff0
John Riley <jhon_aiox@hotmail.com>
2019-03-19 15:30:32 -0300

    ~correções no cadastro de exploração florestal

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno.Extensoes/Caracterizacoes/ModuloCaracterizacao/eProduto.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Entities/Interno.Extensoes/Caracterizacoes/ModuloCaracterizacao/eProduto.cs
@@ -3,11 +3,14 @@
 	public enum eProduto
 	{
 		Lenha = 1,
-		Toras,
-		Toretes,
-		MouroesEstacas,
-		Escoras,
-		Palmito,
-		SemRendimento
+		Toras = 2,
+		Toretes = 3,
+		MouroesEstacas = 4,
+		Escoras = 5,
+		Palmito = 6,
+		SemRendimento = 7,
+		Casca = 8,
+		Folhas = 9,
+		MudaPlanta = 10
 	}
 }
