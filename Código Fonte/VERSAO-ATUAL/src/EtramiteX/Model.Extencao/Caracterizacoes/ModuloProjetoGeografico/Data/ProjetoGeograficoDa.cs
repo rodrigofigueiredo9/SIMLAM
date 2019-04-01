5cb4f7243b07c3f5f074ae3488f7a338fa9e00ab
John Riley <jhon_aiox@hotmail.com>
2019-04-01 01:30:45 -0300

    ajuste no processamento geográfico LVFL

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model.Extencao/Caracterizacoes/ModuloProjetoGeografico/Data/ProjetoGeograficoDa.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model.Extencao/Caracterizacoes/ModuloProjetoGeografico/Data/ProjetoGeograficoDa.cs
@@ -554,7 +554,7 @@ namespace Tecnomapas.EtramiteX.Interno.Model.Extensoes.Caracterizacoes.ModuloPro
 					end if;
 
 					insert into {1}DES_PATIV (id, projeto, cod_apmp, codigo, atividade, rocha, massa_dagua, avn, aa, afs, rest_declividade, arl, rppn, app, tipo_exploracao, geometry)
-						(select a.id, a.projeto, a.cod_apmp, a.codigo, a.atividade, a.rocha, a.massa_dagua, a.avn, a.aa, a.afs, a.rest_declividade, a.arl, a.rppn, a.app, a.tipo_exploracao, a.geometry
+						(select seq_des_pativ.nextval, a.projeto, a.cod_apmp, a.codigo, a.atividade, a.rocha, a.massa_dagua, a.avn, a.aa, a.afs, a.rest_declividade, a.arl, a.rppn, a.app, a.tipo_exploracao, a.geometry
 							from {1}GEO_PATIV a where a.projeto = :projeto
 							and exists
 							(select 1 from {0}crt_exp_florestal_geo g
@@ -563,10 +563,9 @@ namespace Tecnomapas.EtramiteX.Interno.Model.Extensoes.Caracterizacoes.ModuloPro
 								where ep.id = g.exp_florestal_exploracao
 								and exists(select 1 from {0}tab_titulo_exp_florestal t
 								where t.titulo = :titulo
-								and t.exploracao_florestal = ep.exploracao_florestal)))
-							and not exists(select 1 from {1}DES_PATIV d where d.id = a.id));
+								and t.exploracao_florestal = ep.exploracao_florestal))));
 					insert into {1}DES_AATIV (id, projeto, area_m2, cod_apmp, codigo, atividade, rocha, massa_dagua, avn, aa, afs, rest_declividade, arl, rppn, app, tipo_exploracao, geometry)
-						(select a.id, a.projeto, a.area_m2, a.cod_apmp, a.codigo, a.atividade, a.rocha, a.massa_dagua, a.avn, a.aa, a.afs, a.rest_declividade, a.arl, a.rppn, a.app, a.tipo_exploracao, a.geometry
+						(select seq_des_aativ.nextval, a.projeto, a.area_m2, a.cod_apmp, a.codigo, a.atividade, a.rocha, a.massa_dagua, a.avn, a.aa, a.afs, a.rest_declividade, a.arl, a.rppn, a.app, a.tipo_exploracao, a.geometry
 							from {1}GEO_AATIV a where a.projeto = :projeto
 							and exists
 							(select 1 from {0}crt_exp_florestal_geo g
@@ -575,8 +574,7 @@ namespace Tecnomapas.EtramiteX.Interno.Model.Extensoes.Caracterizacoes.ModuloPro
 								where ep.id = g.exp_florestal_exploracao
 								and exists(select 1 from {0}tab_titulo_exp_florestal t
 								where t.titulo = :titulo
-								and t.exploracao_florestal = ep.exploracao_florestal)))
-							and not exists(select 1 from {1}DES_AATIV d where d.id = a.id));
+								and t.exploracao_florestal = ep.exploracao_florestal))));
 
 					delete from {1}GEO_PATIV a where a.projeto = :projeto
 						and exists
