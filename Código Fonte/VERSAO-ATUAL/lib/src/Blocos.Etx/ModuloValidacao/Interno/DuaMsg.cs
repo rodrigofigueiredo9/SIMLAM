029a885db7ef96b1239805f707637d09f1536e95
John Riley <jhon_aiox@hotmail.com>
2019-04-25 16:05:42 -0300

    correcoes informacao de corte

--- Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Etx/ModuloValidacao/Interno/DuaMsg.cs
+++ Código Fonte/VERSAO-ATUAL/lib/src/Blocos.Etx/ModuloValidacao/Interno/DuaMsg.cs
@@ -7,6 +7,7 @@ namespace Tecnomapas.Blocos.Etx.ModuloValidacao
 	public partial class Mensagem
 	{
 		private static DuaMsg _DuaMsg = new DuaMsg();
+
 		public static DuaMsg Dua
 		{
 			get { return _DuaMsg; }
@@ -15,11 +16,29 @@ namespace Tecnomapas.Blocos.Etx.ModuloValidacao
 
 	public class DuaMsg
 	{
-		public Mensagem ExisteDuaTitulo { get { return new Mensagem() { Tipo = eTipoMensagem.Advertencia, Texto = "Não é possível editar esse título, porque todos os DUAS para esse título já foram emitidos." }; } }
-		public Mensagem SolicitacaoSituacaoNaoPodeEditar (string situacaoAtual) 
+		public Mensagem Invalido(string numero)
 		{
-			return new Mensagem() { Tipo = eTipoMensagem.Advertencia, Campo = "Solicitacao_Situacao", Texto = String.Format("Não é possível editar a Solicitação de inscrição na situação \"{0}\".", situacaoAtual) }; 
-		} 
-	
+			return new Mensagem() { Texto = String.Format("O DUA {0} não é valido.", numero), Tipo = eTipoMensagem.Advertencia };
+		}
+		public Mensagem SemSaldo(string numero)
+		{
+			return new Mensagem() { Texto = String.Format("O DUA {0} não possui saldo.", numero), Tipo = eTipoMensagem.Advertencia };
+		}
+
+		public Mensagem NaoEncontrado { get { return new Mensagem() { Campo = "", Texto = "Dua não encontrado.", Tipo = eTipoMensagem.Advertencia }; } }
+		public Mensagem ErroSefaz(string xMotivo) { return new Mensagem() { Campo = "", Texto = "Sefaz informa: " + xMotivo, Tipo = eTipoMensagem.Advertencia }; }
+		public Mensagem ErroAoConsultar { get { return new Mensagem() { Campo = "", Texto = "As tentativas de acesso ao serviço do DUA foram esgotadas, por favor entre em contato com o administrador do sistema.", Tipo = eTipoMensagem.Advertencia }; } }
+		public Mensagem NumeroObrigatorio { get { return new Mensagem() { Campo = "NumeroDua", Texto = "Número do DUA é Obrigatório.", Tipo = eTipoMensagem.Advertencia }; } }
+		public Mensagem CPFCNPJObrigatorio { get { return new Mensagem() { Campo = "CPFCNPJDUA, .txtCNPJDUA", Texto = "CPF/CNPJ é Obrigatório.", Tipo = eTipoMensagem.Advertencia }; } }
+		public Mensagem CodigoInvalido { get { return new Mensagem() { Texto = "Código do DUA inválido.", Tipo = eTipoMensagem.Advertencia }; } }
+
+		public Mensagem Sucesso { get { return new Mensagem() { Tipo = eTipoMensagem.Sucesso, Texto = "DUAs emitidos com sucesso." }; } }
+		public Mensagem Falha { get { return new Mensagem() { Tipo = eTipoMensagem.Advertencia, Texto = "Falha ao emitir DUAs." }; } }
+		public Mensagem ExisteDuaTitulo { get { return new Mensagem() { Tipo = eTipoMensagem.Advertencia, Texto = "Não é possível editar esse título, porque todos os DUAS para esse título já foram emitidos." }; } }
+		public Mensagem SolicitacaoSituacaoNaoPodeEditar(string situacaoAtual)
+		{
+			return new Mensagem() { Tipo = eTipoMensagem.Advertencia, Campo = "Solicitacao_Situacao", Texto = String.Format("Não é possível editar a Solicitação de inscrição na situação \"{0}\".", situacaoAtual) };
+		}
+
 	}
 }
