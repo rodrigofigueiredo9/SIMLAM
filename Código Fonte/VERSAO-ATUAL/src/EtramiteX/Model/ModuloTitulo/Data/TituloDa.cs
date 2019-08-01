6e7c5f128a6df8501b7cdb3862556fb434327944
cinthia.aguiar <cinthia.aguiar@pentago.com.br>
2019-08-01 12:23:12 -0300

    Correção em nome de coluna do relatório.

--- Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloTitulo/Data/TituloDa.cs
+++ Código Fonte/VERSAO-ATUAL/src/EtramiteX/Model/ModuloTitulo/Data/TituloDa.cs
@@ -2678,7 +2678,7 @@ namespace Tecnomapas.EtramiteX.Interno.Model.ModuloTitulo.Data
 					"vazao_max_adequacoes",
 					"periodo_inicio_obra",
 				   "periodo_termino_obra",
-					"atividades",
+					"finalidades",
 					"rt_" + eTipoRT.ElaboracaoDeclaracao.Description() + "_nome",
 					"rt_" + eTipoRT.ElaboracaoDeclaracao.Description() + "_profissao",
 					"rt_" + eTipoRT.ElaboracaoDeclaracao.Description() + "_registro_crea",
